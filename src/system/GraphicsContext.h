4801fcf9e04c9de4f9cd7eedd42d8c2a95f6428f
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-08-23 21:42:51 +0200

    Added some documentation

--- src/system/GraphicsContext.h
+++ src/system/GraphicsContext.h
@@ -6,18 +6,35 @@
 #include <memory>
 #include <stdint.h>
 
-
+/// The graphics context interface used by the game.
+///
+/// The implementation's task is to provide a graphical window, and create drawable textures,
+/// either by loading image files, or by text rendering.
+///
+/// For testing purposes, it should also provide a way to save screenshots of the game window.
 class GraphicsContext {
 public:
+    /// Called at the end of every frame, `render()` should update
+    /// the displayed image of the game window.
     virtual void render() = 0;
+
+    /// Toggle fullscreen mode, if supported.
     virtual void toggleFullscreen() = 0;
     virtual uint16_t screenWidth() = 0;
     virtual uint16_t screenHeight() = 0;
 
+    /// Load a font file (in OTF or TTF format) in the given size.
+    /// The client should be able to use the same `ResourceID` later to refer to this font.
     virtual void loadFont(ResourceID, const std::string& path, unsigned pt) = 0;
+
+    /// Render text to a texture, using a previously loaded font,
+    /// using the provided RGBA color.
+    /// Later, the client should be able to refer and draw this texture, using its `ResourceID`.
+    /// The implementation should cache the created texture, if possible.
     virtual void cacheText(ResourceID, const std::string& text,
                            ResourceID font_id, const std::array<uint8_t, 4>& color) = 0;
 
+    /// Draw a previously created texture, referred with a `ResourceID`, at coords (0;0)
     virtual void drawTexture(ResourceID) = 0;
 
     virtual void saveScreenshotBMP(const std::string& path) = 0;
