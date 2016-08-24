a184684a48ea4ea1d78d599b612fed4eafaaa1d4
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-08-25 00:30:07 +0200

    Changed regular text test strings

--- tests/test_GraphicsContext.cpp
+++ tests/test_GraphicsContext.cpp
@@ -3,6 +3,8 @@
 #include "system/GraphicsContext.h"
 
 
+const std::string SCREENSHOT_NAME = "test.bmp";
+
 class AppContext {
 public:
     AppContext()
@@ -17,6 +19,7 @@ protected:
 enum class ResourceID: uint8_t {
     TEST_FONT,
     TEST_TEX_ASCII,
+    TEST_TEX_LATIN1,
     TEST_TEX_LATIN2,
     TEST_TEX_CYRILLIC,
     TEST_TEX_JAPANESE,
@@ -28,26 +31,34 @@ TEST_FIXTURE(AppContext, Create) {
 }
 
 TEST_FIXTURE(AppContext, SaveScreenshot) {
-    gcx->requestScreenshot("test_screen.bmp");
+    gcx->requestScreenshot(SCREENSHOT_NAME);
     gcx->render();
 }
 
 TEST_FIXTURE(AppContext, TextRendering) {
-    gcx->loadFont(ResourceID::TEST_FONT, "data/regular.otf", 30);
+    gcx->loadFont(ResourceID::TEST_FONT, "data/regular.otf", 20);
+
+    // The regular font must support all english characters
     gcx->cacheText(ResourceID::TEST_TEX_ASCII, "The quick brown fox jumped over the lazy dog",
                    ResourceID::TEST_FONT, {0xFF, 0xFF, 0xFF, 0xFF});
-    gcx->cacheText(ResourceID::TEST_TEX_LATIN2, "Árvíztűrő tükörfúrógép",
+    // Some games have german or greek letter (eg. über, alpha)
+    gcx->cacheText(ResourceID::TEST_TEX_LATIN1, "Zwölf Boxkämpfer über den großen αβγδεφχψω ΔΣΦΨΩ",
                    ResourceID::TEST_FONT, {0xFF, 0xFF, 0xFF, 0xFF});
+    // TODO: for future localization, add Latin2 support
+    gcx->cacheText(ResourceID::TEST_TEX_LATIN2, "Árvíztűrő tükörfúrógép Pchnąć w tę łódź",
+                   ResourceID::TEST_FONT, {0xFF, 0xFF, 0xFF, 0xFF});
+    // TODO: for future localization, add non-latin language support
     gcx->cacheText(ResourceID::TEST_TEX_CYRILLIC, "Широкая электрификация южных",
                    ResourceID::TEST_FONT, {0xFF, 0xFF, 0xFF, 0xFF});
-    gcx->cacheText(ResourceID::TEST_TEX_JAPANESE, "色は匂へど散りぬるを",
+    // The regular font must support hiragana, katakana and the most common kanjis
+    gcx->cacheText(ResourceID::TEST_TEX_JAPANESE, "色は匂へど散りぬるを イロハニホヘト 鳥啼く声す 夢覚ませ",
                    ResourceID::TEST_FONT, {0xFF, 0xFF, 0xFF, 0xFF});
 
-    // TODO
     gcx->drawTexture(ResourceID::TEST_TEX_ASCII, 10, 10);
-    gcx->drawTexture(ResourceID::TEST_TEX_LATIN2, 10, 50);
-    gcx->drawTexture(ResourceID::TEST_TEX_CYRILLIC, 10, 90);
-    // gcx->drawTexture(ResourceID::TEST_TEX_JAPANESE, 10, 130);
-    gcx->requestScreenshot("test_screen.bmp");
+    gcx->drawTexture(ResourceID::TEST_TEX_LATIN1, 10, 50);
+    // gcx->drawTexture(ResourceID::TEST_TEX_LATIN2, 10, 90);
+    // gcx->drawTexture(ResourceID::TEST_TEX_CYRILLIC, 10, 130);
+    gcx->drawTexture(ResourceID::TEST_TEX_JAPANESE, 10, 170);
+    gcx->requestScreenshot(SCREENSHOT_NAME);
     gcx->render();
 }
