95e335fddd0d266fa2cd6f9f12355fa9420c5e52
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-09-15 23:46:31 +0200

    Added Animation class

---
+++ src/game/Animation.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <assert.h>
+
+
+class AnimationBase {
+public:
+    using Duration = std::chrono::steady_clock::duration;
+
+    /// Returns true if the animation did not finish yet.
+    virtual bool running() const { return is_running; }
+
+protected:
+    AnimationBase(Duration duration, std::function<void()> on_end)
+        : duration(duration)
+        , timer(Duration::zero())
+        , end_callback(on_end)
+        , is_running(true)
+    {
+        assert(duration > Duration::zero());
+    }
+
+    Duration duration;
+    Duration timer;
+    std::function<void()> end_callback;
+    bool is_running;
+};
+
+/// An Animation is responsible for repeatedly calling a specified function
+/// for a duration of time, with the percent of already passed time as parameter.
+template<typename T>
+class Animation : public AnimationBase {
+public:
+    /// Crates a new animation witha specified duration, animation function,
+    /// and an optional callback which would be invoked on completion.
+    ///
+    /// The animation function receives the elapsed time as a value in the range 0.0-1.0.
+    /// If it has a return value, the result will be stored, and can be later queried by
+    /// the Animation's value() member.
+    ///
+    /// When creating the Animation, on_update will be called with the value 0.0 .
+    /// After the elapsed time reaches the duration (by repeatedly calling update()), on_update will
+    /// be called with the value 1.0, then on_end_cb will be called,
+    /// after that the animation stops and never calls any of the functions again.
+    Animation(Duration duration, std::function<T(double)> on_update, std::function<void()> on_end = [](){})
+        : AnimationBase(duration, on_end)
+        , animator(on_update)
+        , last_value(animator(0.0))
+    {}
+
+    /// Update the animation's timer with the specified amount of time.
+    /// The animator function will be called.
+    void update(Duration elapsed) {
+        if (!is_running)
+            return;
+
+        timer += elapsed;
+        if (timer >= duration) {
+            is_running = false;
+            last_value = animator(1.0);
+            end_callback();
+        }
+        else
+            last_value = animator(timer / (duration * 1.0));
+    }
+
+    /// Returns the last result of the animation function
+    const T& value() const { return last_value; }
+
+private:
+    std::function<T(double)> animator;
+    T last_value;
+};
+
+template<>
+class Animation<void> : public AnimationBase {
+    using Duration = std::chrono::steady_clock::duration;
+
+public:
+    Animation(Duration duration, std::function<void(double)> on_update, std::function<void()> on_end = [](){})
+        : AnimationBase(duration, on_end)
+        , animator(on_update)
+    {
+        on_update(0.0);
+    }
+
+    void update(Duration elapsed) {
+        if (!is_running)
+            return;
+
+        timer += elapsed;
+        if (timer >= duration) {
+            is_running = false;
+            animator(1.0);
+            end_callback();
+        }
+        else
+            animator(timer / (duration * 1.0));
+    }
+
+    void value() const {};
+
+private:
+    std::function<void(double)> animator;
+};
---
+++ tests/test_Animation.cpp
@@ -0,0 +1,94 @@
+#include "UnitTest++/UnitTest++.h"
+
+#include "game/Animation.h"
+
+#include <cmath>
+
+
+SUITE(Animation) {
+    using namespace std::chrono_literals;
+
+TEST(CreateVoid) {
+    bool val = false;
+
+    Animation<void> anim(1s, [&val](double){ val = true; });
+    CHECK_EQUAL(true, anim.running());
+    CHECK_EQUAL(true, val);
+
+    val = false;
+    anim.update(1s);
+    CHECK_EQUAL(true, val);
+    CHECK_EQUAL(false, anim.running());
+
+    // never calls again
+    val = false;
+    anim.update(1s);
+    CHECK_EQUAL(false, val);
+    CHECK_EQUAL(false, anim.running());
+}
+
+TEST(CreateT) {
+    bool val = false;
+
+    Animation<int> anim(1s, [&val](double){ val = true; return 0; });
+    CHECK_EQUAL(true, anim.running());
+    CHECK_EQUAL(true, val);
+
+    val = false;
+    anim.update(1s);
+    CHECK_EQUAL(true, val);
+    CHECK_EQUAL(false, anim.running());
+
+    // never calls again
+    val = false;
+    anim.update(1s);
+    CHECK_EQUAL(false, val);
+    CHECK_EQUAL(false, anim.running());
+}
+
+TEST(AnimatorRunsMultipleTimes) {
+    int val = 0;
+
+    Animation<void> anim_bind(3s, [&val](double t){ val = std::round(t * 3); });
+    CHECK_EQUAL(0, val);
+    anim_bind.update(1s);
+    CHECK_EQUAL(1, val);
+    anim_bind.update(1s);
+    CHECK_EQUAL(2, val);
+    anim_bind.update(1s);
+    CHECK_EQUAL(3, val);
+
+    Animation<int> anim_inner(3s, [](double t){ return std::round(t * 3); });
+    CHECK_EQUAL(0, anim_inner.value());
+    anim_inner.update(1s);
+    CHECK_EQUAL(1, anim_inner.value());
+    anim_inner.update(1s);
+    CHECK_EQUAL(2, anim_inner.value());
+    anim_inner.update(1s);
+    CHECK_EQUAL(3, anim_inner.value());
+}
+
+TEST(OnEndCallbackIsCalled) {
+    int val = 0;
+    bool finished = false;
+
+    Animation<void> anim_bind(3s, [&val](double t){ val = std::round(t * 3); }, [&finished](){ finished = true; });
+    CHECK_EQUAL(0, val);
+    CHECK_EQUAL(false, finished);
+    anim_bind.update(1s);
+    CHECK_EQUAL(1, val);
+    CHECK_EQUAL(false, finished);
+    anim_bind.update(1s);
+    CHECK_EQUAL(2, val);
+    CHECK_EQUAL(false, finished);
+    anim_bind.update(1s);
+    CHECK_EQUAL(3, val);
+    CHECK_EQUAL(true, finished);
+
+    // never call again
+    anim_bind.update(1s);
+    CHECK_EQUAL(3, val);
+    CHECK_EQUAL(true, finished);
+}
+
+} // Suite
