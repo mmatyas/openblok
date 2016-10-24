b261b31f8e14026237ad8fe47207463005a80e30
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-10-24 17:41:37 +0200

    Added another T-Spin test

--- tests/test_WellTSpin.cpp
+++ tests/test_WellTSpin.cpp
@@ -75,4 +75,56 @@ TEST_FIXTURE(WellFixture, ThreeCornerBasic)
     CHECK_EQUAL(true, tspin_detected);
 }
 
+TEST_FIXTURE(WellFixture, ThreeCornerTricky)
+{
+    bool tspin_detected = false;
+    well.registerObserver(WellEvent::Type::TSPIN_DETECTED, [&tspin_detected](const WellEvent&){
+        tspin_detected = true;
+    });
+
+    std::string base_ascii;
+    for (unsigned i = 0; i < 17; i++)
+        base_ascii += emptyline_ascii;
+    base_ascii += "......OOOO\n";
+    base_ascii += ".......OOO\n";
+    base_ascii += "OOOOOO.OOO\n";
+    base_ascii += "OOOOO..OOO\n";
+    base_ascii += "OOOOOO.OOO\n";
+
+    well.fromAscii(base_ascii);
+    well.addPiece(PieceType::T);
+
+    // move to bottom
+    for (unsigned row = 0; row < 20 * softdrop_delay_frames; row++) {
+        well.update({InputEvent(InputType::GAME_SOFTDROP, true)}, app);
+    }
+    well.update({InputEvent(InputType::GAME_SOFTDROP, false)}, app);
+    CHECK(well.activePiece() != nullptr);
+
+    // move right
+    well.update({InputEvent(InputType::GAME_MOVE_RIGHT, true)}, app);
+    well.update({InputEvent(InputType::GAME_MOVE_RIGHT, false)}, app);
+
+    // rotate CCW
+    well.update({InputEvent(InputType::GAME_ROTATE_LEFT, true)}, app);
+    well.update({InputEvent(InputType::GAME_ROTATE_LEFT, false)}, app);
+
+    // check position
+    std::string expected_ascii;
+    for (unsigned i = 0; i < 17; i++)
+        expected_ascii += emptyline_ascii;
+    expected_ascii += "......OOOO\n";
+    expected_ascii += ".......OOO\n";
+    expected_ascii += "OOOOOOtOOO\n";
+    expected_ascii += "OOOOOttOOO\n";
+    expected_ascii += "OOOOOOtOOO\n";
+    CHECK_EQUAL(expected_ascii, well.asAscii());
+
+    // wait for lock
+    for (unsigned i = 0; i < lock_delay_frames; i++)
+        well.update({}, app);
+
+    CHECK_EQUAL(true, tspin_detected);
+}
+
 } // Suite
