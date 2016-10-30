29f416568b16b517d09022a5ae9cf735d93eb318
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-10-30 13:33:45 +0100

    Updated graphics context tests
    They are more like a documentation, it's probably overkill to
    check every feature of the graphics API to such detail.

--- tests/test_GraphicsContext.cpp
+++ tests/test_GraphicsContext.cpp
@@ -1,40 +1,26 @@
 #include "UnitTest++/UnitTest++.h"
 
 #include "TestUtils.h"
+#include "system/Font.h"
+#include "system/Window.h"
 #include "system/GraphicsContext.h"
+#include "system/Texture.h"
 
-
-const std::string SCREENSHOT_NAME = std::tmpnam(nullptr);
 
 class AppContext {
 public:
     AppContext()
-        : gcx(GraphicsContext::create())
+        : window(Window::create())
     {
         // TODO: set window and resolution to 640x480
     }
 
+    GraphicsContext& gcx() {
+        return window->graphicsContext();
+    }
+
 protected:
-    std::unique_ptr<GraphicsContext> gcx;
-};
-
-
-enum class FontID: uint8_t {
-    REGULAR,
-};
-
-enum class TexID: uint8_t {
-    GREENRECT,
-
-    // Test 1
-    T1_ASCII,
-    T1_LATIN1,
-    T1_LATIN2,
-    T1_CYRILLIC,
-    T1_JAPANESE,
-
-    // Test 2
-    T2_LINEBREAK,
+    std::unique_ptr<Window> window;
 };
 
 
@@ -45,88 +31,92 @@ TEST_FIXTURE(AppContext, Create) {
 }
 
 TEST_FIXTURE(AppContext, SaveScreenshot) {
-    gcx->requestScreenshot(SCREENSHOT_NAME);
-    gcx->render();
+    window->requestScreenshot(std::tmpnam(nullptr));
+    gcx().render();
 }
 
 TEST_FIXTURE(AppContext, TextRendering) {
-    gcx->loadFont(FontID::REGULAR, "data/regular.otf", 20);
+    auto font = gcx().loadFont("data/fonts/PTC75F.ttf", 20);
 
     // The regular font must support all english characters
-    gcx->cacheText(TexID::T1_ASCII, "The quick brown fox jumped over the lazy dog",
-                   FontID::REGULAR, 0xFFFFFF_rgb);
+    auto ascii = font->renderText("The quick brown fox jumped over the lazy dog", 0xFFFFFF_rgb);
     // Some games have german or greek letter (eg. über, alpha)
-    gcx->cacheText(TexID::T1_LATIN1, "Zwölf Boxkämpfer über den großen αβγδεφχψω ΔΣΦΨΩ",
-                   FontID::REGULAR, 0xFFFFFF_rgb);
+    auto latin1 = font->renderText("Zwölf Boxkämpfer über den großen αβγδεφχψω ΔΣΦΨΩ", 0xFFFFFF_rgb);
     // TODO: for future localization, add Latin2 support
-    gcx->cacheText(TexID::T1_LATIN2, "Árvíztűrő tükörfúrógép Pchnąć w tę łódź",
-                   FontID::REGULAR, 0xFFFFFF_rgb);
+    auto latin2 = font->renderText("Árvíztűrő tükörfúrógép Pchnąć w tę łódź", 0xFFFFFF_rgb);
     // TODO: for future localization, add non-latin language support
-    gcx->cacheText(TexID::T1_CYRILLIC, "Широкая электрификация южных",
-                   FontID::REGULAR, 0xFFFFFF_rgb);
+    auto cyrillic = font->renderText("Широкая электрификация южных", 0xFFFFFF_rgb);
     // The regular font must support hiragana, katakana and the most common kanjis
-    gcx->cacheText(TexID::T1_JAPANESE, "色は匂へど散りぬるを イロハニホヘト 鳥啼く声す 夢覚ませ",
-                   FontID::REGULAR, 0xFFFFFF_rgb);
+    auto japanese = font->renderText("色は匂へど散りぬるを イロハニホヘト 鳥啼く声す 夢覚ませ", 0xFFFFFF_rgb);
 
-    gcx->drawTexture(TexID::T1_ASCII, 10, 10);
-    gcx->drawTexture(TexID::T1_LATIN1, 10, 50);
-    // gcx->drawTexture(TexID::T1_LATIN2, 10, 90);
-    // gcx->drawTexture(TexID::T1_CYRILLIC, 10, 130);
-    gcx->drawTexture(TexID::T1_JAPANESE, 10, 170);
-    gcx->requestScreenshot(SCREENSHOT_NAME);
-    gcx->render();
+    ascii->drawAt(10, 10);
+    latin1->drawAt(10, 50);
+    latin2->drawAt(10, 90);
+    cyrillic->drawAt(10, 130);
+    japanese->drawAt(10, 170);
 
-    CHECK(TestUtils::imageCompare("tests/references/text_multilang.png", SCREENSHOT_NAME));
+    const std::string screenshot_path = std::tmpnam(nullptr);
+    window->requestScreenshot(screenshot_path);
+    gcx().render();
+
+    CHECK(TestUtils::imageCompare("tests/references/text_multilang.png", screenshot_path));
 }
 
 TEST_FIXTURE(AppContext, TextLinebreak) {
-    gcx->loadFont(FontID::REGULAR, "data/regular.otf", 30);
-    gcx->cacheText(TexID::T2_LINEBREAK, "There should be\nthree lines\non the screen",
-                   FontID::REGULAR, 0xFFFFFF_rgb);
+    auto font = gcx().loadFont("data/fonts/PTC75F.ttf", 30);
+    auto tex = font->renderText("There should be\nthree lines\non the screen", 0xFFFFFF_rgb);
 
-    gcx->drawTexture(TexID::T2_LINEBREAK, 10, 10);
-    gcx->requestScreenshot(SCREENSHOT_NAME);
-    gcx->render();
+    tex->drawAt(10, 10);
 
-    CHECK(TestUtils::imageCompare("tests/references/text_multiline.png", SCREENSHOT_NAME));
+    const std::string screenshot_path = std::tmpnam(nullptr);
+    window->requestScreenshot(screenshot_path);
+    gcx().render();
+
+    CHECK(TestUtils::imageCompare("tests/references/text_multiline.png", screenshot_path));
 }
 
 TEST_FIXTURE(AppContext, TextEmpty) {
-    gcx->loadFont(FontID::REGULAR, "data/regular.otf", 30);
+    auto font = gcx().loadFont("data/fonts/PTC75F.ttf", 30);
 
     CHECK_THROW(
-        gcx->cacheText(TexID::T2_LINEBREAK, "",
-                       FontID::REGULAR, 0xFFFFFF_rgb),
+        font->renderText("", 0xFFFFFF_rgb),
         std::runtime_error
     );
 }
 
 TEST_FIXTURE(AppContext, DrawRect) {
-    gcx->drawFilledRect({10, 20, 120, 100}, 0xFF0000_rgb);
-    gcx->drawFilledRect({50, 60, 120, 100}, 0x00FF00_rgb);
-    gcx->drawFilledRect({100, 110, 120, 100}, 0x0000FF_rgb);
-    gcx->requestScreenshot(SCREENSHOT_NAME);
-    gcx->render();
+    gcx().drawFilledRect({10, 20, 120, 100}, 0xFF0000_rgb);
+    gcx().drawFilledRect({50, 60, 120, 100}, 0x00FF00_rgb);
+    gcx().drawFilledRect({100, 110, 120, 100}, 0x0000FF_rgb);
 
-    CHECK(TestUtils::imageCompare("tests/references/draw_filledrect.png", SCREENSHOT_NAME));
+    const std::string screenshot_path = std::tmpnam(nullptr);
+    window->requestScreenshot(screenshot_path);
+    gcx().render();
+
+    CHECK(TestUtils::imageCompare("tests/references/draw_filledrect.png", screenshot_path));
 }
 
 TEST_FIXTURE(AppContext, LoadImage) {
-    gcx->loadTexture(TexID::GREENRECT, "tests/data/green_rect.png");
-    gcx->drawTexture(TexID::GREENRECT, 10, 10);
-    gcx->requestScreenshot(SCREENSHOT_NAME);
-    gcx->render();
+    auto tex = gcx().loadTexture("tests/data/green_rect.png");
+    tex->drawAt(10, 10);
 
-    CHECK(TestUtils::imageCompare("tests/references/draw_image.png", SCREENSHOT_NAME));
+    const std::string screenshot_path = std::tmpnam(nullptr);
+    window->requestScreenshot(screenshot_path);
+    gcx().render();
+
+
+    CHECK(TestUtils::imageCompare("tests/references/draw_image.png", screenshot_path));
 }
 
 TEST_FIXTURE(AppContext, DrawScaled) {
-    gcx->loadTexture(TexID::GREENRECT, "tests/data/green_rect.png");
-    gcx->drawTexture(TexID::GREENRECT, {10, 20, 50, 50});
-    gcx->requestScreenshot(SCREENSHOT_NAME);
-    gcx->render();
+    auto tex = gcx().loadTexture("tests/data/green_rect.png");
+    tex->drawScaled({10, 20, 50, 50});
 
-    CHECK(TestUtils::imageCompare("tests/references/draw_scaled.png", SCREENSHOT_NAME));
+    const std::string screenshot_path = std::tmpnam(nullptr);
+    window->requestScreenshot(screenshot_path);
+    gcx().render();
+
+    CHECK(TestUtils::imageCompare("tests/references/draw_scaled.png", screenshot_path));
 }
 
 } // SUITE
