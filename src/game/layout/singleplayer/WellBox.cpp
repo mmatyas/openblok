16657ec9dfb6b9879f5a81bd842c4ff2ba9b5403
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-11-05 17:09:46 +0100

    Changed countdown font

--- src/game/layout/singleplayer/WellBox.cpp
+++ src/game/layout/singleplayer/WellBox.cpp
@@ -35,7 +35,7 @@ WellBox::WellBox(AppContext& app)
     tex_pause = font_big->renderText(tr("PAUSE"), 0xEEEEEE_rgb);
     tex_gameover = font_big->renderText(tr("GAME OVER"), 0xEEEEEE00_rgba);
 
-    auto font_huge = app.gcx().loadFont("data/fonts/PTC75F.ttf", 120);
+    auto font_huge = app.gcx().loadFont("data/fonts/helsinki.ttf", 150);
     tex_countdown = {
         font_huge->renderText(tr("3"), 0xEEEEEE_rgb),
         font_huge->renderText(tr("2"), 0xEEEEEE_rgb),
