086a3c66ff85fc97311b3be4c1daf30569fba864
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-11-24 23:33:05 +0100

    Colors are now `constexpr`s

--- src/system/Color.h
+++ src/system/Color.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <assert.h>
 #include <stdint.h>
 
 
@@ -12,5 +13,20 @@ struct RGBAColor {
 };
 
 // C++11 custom literals
-RGBColor operator "" _rgb(unsigned long long color);
-RGBAColor operator "" _rgba(unsigned long long color);
+constexpr RGBColor operator "" _rgb(unsigned long long color) {
+    assert(color <= 0xFFFFFF); // RGB numeric value should be under 0xFFFFFF
+    return {
+        static_cast<uint8_t>((color & 0xFF0000) >> 16),
+        static_cast<uint8_t>((color & 0xFF00) >> 8),
+        static_cast<uint8_t>(color & 0xFF)
+    };
+}
+constexpr RGBAColor operator "" _rgba(unsigned long long color) {
+    assert(color <= 0xFFFFFFFF); // RGBA numeric value should be under 0xFFFFFFFF
+    return {
+        static_cast<uint8_t>((color & 0xFF000000) >> 24),
+        static_cast<uint8_t>((color & 0xFF0000) >> 16),
+        static_cast<uint8_t>((color & 0xFF00) >> 8),
+        static_cast<uint8_t>(color & 0xFF)
+    };
+}
