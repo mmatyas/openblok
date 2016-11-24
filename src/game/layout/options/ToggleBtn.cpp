8d832d3b8b1017a4f5af9a0a6696b4fdbb0c97f6
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-11-24 23:18:11 +0100

    Minor optimization: do not calc toggle switch locations in every draw call

--- src/game/layout/options/ToggleBtn.cpp
+++ src/game/layout/options/ToggleBtn.cpp
@@ -19,10 +19,10 @@ ToggleButton::ToggleButton(AppContext& app,
     , switch_state(initial_state)
     , callback(on_toggle)
 {
-    bounding_box.w = 750;
     bounding_box.h = 60;
+    setWidth(750);
 
-    padding = (height() - tex_label->height()) / 2;
+    padding_ver = (height() - tex_label->height()) / 2;
 
     auto font = app.gcx().loadFont(Paths::data() + "fonts/PTS55F.ttf", 28);
     tex_onoff.at(0) = font->renderText(tr("OFF"), 0xEEEEEE_rgb);
@@ -33,6 +33,13 @@ void ToggleButton::setPosition(int x, int y)
 {
     bounding_box.x = x;
     bounding_box.y = y;
+}
+
+void ToggleButton::setWidth(int w)
+{
+    OptionsItem::setWidth(w);
+    rightswitch_centerx = x() + width() - padding_hor - 50;
+    leftswitch_centerx = rightswitch_centerx - 110;
 }
 
 void ToggleButton::onLeftPress()
@@ -53,27 +60,23 @@ void ToggleButton::onRightPress()
 
 void ToggleButton::draw(GraphicsContext& gcx) const
 {
-    static const int padding_hor = 20;
     if (is_active) {
         gcx.drawFilledRect(bounding_box, 0x0060BF_rgb);
         gcx.drawFilledRect({x(), y(), 6, height()}, 0xCE8000_rgb);
-        tex_label->drawAt(x() + padding_hor, y() + padding);
+        tex_label->drawAt(x() + padding_hor, y() + padding_ver);
     }
     else {
         gcx.drawFilledRect(bounding_box, 0x002687_rgb);
-        tex_label->drawAt(x() + padding_hor, y() + padding);
+        tex_label->drawAt(x() + padding_hor, y() + padding_ver);
     }
-
-    const int rightswitch_centerx = x() + width() - padding_hor - 50;
-    const int leftswitch_centerx = rightswitch_centerx - 110;
 
     if (switch_state)
         gcx.drawFilledRect({rightswitch_centerx - 50, y() + 5, 100, 50}, 0xCE8000_rgb);
     else
         gcx.drawFilledRect({leftswitch_centerx - 50, y() + 5, 100, 50}, 0xCE8000_rgb);
 
-    tex_onoff.at(0)->drawAt(leftswitch_centerx - tex_onoff.at(0)->width() / 2, y() + padding);
-    tex_onoff.at(1)->drawAt(rightswitch_centerx - tex_onoff.at(1)->width() / 2, y() + padding);
+    tex_onoff.at(0)->drawAt(leftswitch_centerx - tex_onoff.at(0)->width() / 2, y() + padding_ver);
+    tex_onoff.at(1)->drawAt(rightswitch_centerx - tex_onoff.at(1)->width() / 2, y() + padding_ver);
 }
 
 } // namespace Options
--- src/game/layout/options/ToggleBtn.h
+++ src/game/layout/options/ToggleBtn.h
@@ -21,12 +21,17 @@ public:
     void setPosition(int x, int y) override;
     void draw(GraphicsContext&) const override;
 
+    void setWidth(int) override;
     void onLeftPress() override;
     void onRightPress() override;
 
 private:
     std::array<std::unique_ptr<Texture>, 2> tex_onoff;
-    int padding;
+
+    int leftswitch_centerx, rightswitch_centerx;
+    int padding_ver;
+    static constexpr int padding_hor = 20;
+
     bool switch_state;
     std::function<void(bool)> callback;
 };
