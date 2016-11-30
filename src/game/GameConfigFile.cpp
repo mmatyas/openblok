448bd36a322e88cff1e3157873fa45b22f1f76a5
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-11-30 23:57:35 +0100

    Ported GameConfigFile to use the common GameConfig save/load

--- src/game/GameConfigFile.cpp
+++ src/game/GameConfigFile.cpp
@@ -1,5 +1,6 @@
 #include "GameConfigFile.h"
 
+#include "system/ConfigFile.h"
 #include "system/Log.h"
 
 #include <map>
@@ -13,16 +14,14 @@
 
 const std::string LOG_TAG("config");
 
-std::unordered_map<std::string, bool*> createBoolBind(SysConfig& sys)
-{
+std::unordered_map<std::string, bool*> createBoolBind(SysConfig& sys) {
     return {
         {"fullscreen", &sys.fullscreen},
         {"sfx", &sys.sfx},
         {"music", &sys.music},
     };
 }
-std::unordered_map<std::string, bool*> createBoolBind(WellConfig& well)
-{
+std::unordered_map<std::string, bool*> createBoolBind(WellConfig& well) {
     return {
         {"instant_harddrop", &well.instant_harddrop},
         {"tspin_enabled", &well.tspin_enabled},
@@ -30,8 +29,7 @@ std::unordered_map<std::string, bool*> createBoolBind(WellConfig& well)
         {"tspin_allow_kick", &well.tspin_allow_wallkick},
     };
 }
-std::unordered_map<std::string, unsigned short*> createNumericBind(WellConfig& well)
-{
+std::unordered_map<std::string, unsigned short*> createNumericBind(WellConfig& well) {
     return {
         {"das_start", &well.shift_normal},
         {"das_repeat", &well.shift_turbo},
@@ -41,19 +39,19 @@ std::unordered_map<std::string, unsigned short*> createNumericBind(WellConfig& w
 
 const std::set<std::string> accepted_wellenum_keys = {"lock_type", "rotation"};
 const std::unordered_map<std::string, LockDelayType> str_to_locktype {
-    {"\"instant\"", LockDelayType::CLASSIC},
-    {"\"extended\"", LockDelayType::EXTENDED},
-    {"\"infinite\"", LockDelayType::INFINITE},
+    {"instant", LockDelayType::CLASSIC},
+    {"extended", LockDelayType::EXTENDED},
+    {"infinite", LockDelayType::INFINITE},
 };
 const std::unordered_map<std::string, RotationStyle> str_to_rotation {
-    {"\"srs\"", RotationStyle::SRS},
-    {"\"tgm\"", RotationStyle::TGM},
-    {"\"classic\"", RotationStyle::CLASSIC},
+    {"srs", RotationStyle::SRS},
+    {"tgm", RotationStyle::TGM},
+    {"classic", RotationStyle::CLASSIC},
 };
 
-void writeBool(std::ofstream& ofs, const std::string& key, bool value)
+const std::string boolAsStr(bool value)
 {
-    ofs << key << " = " << (value ? "on" : "off") << "\n";
+    return value ? "on" : "off";
 }
 
 void parseBool(bool& config_val, const std::string& config_key, const std::string& str)
@@ -77,137 +75,114 @@ void GameConfigFile::save(SysConfig& sys, WellConfig& well, const std::string& p
         return;
     }
 
-    out << "[system]\n";
+    ConfigFile::Data config;
     {
+        ConfigFile::KeyValPairs sys_entries;
         auto sys_bools = createBoolBind(sys);
         for (const auto& pair : sys_bools)
-            writeBool(out, pair.first, *pair.second);
+            sys_entries.emplace(pair.first, boolAsStr(*pair.second));
+
+        config.emplace("system", std::move(sys_entries));
     }
-    out << "\n";
-    out << "[gameplay]\n";
     {
-        auto well_bools = createBoolBind(well);
+        ConfigFile::KeyValPairs gameplay_entries;
+        auto well_bools = createBoolBind(sys);
         for (const auto& pair : well_bools)
-            writeBool(out, pair.first, *pair.second);
+            gameplay_entries.emplace(pair.first, boolAsStr(*pair.second));
 
         auto well_ushorts = createNumericBind(well);
         for (const auto& pair : well_ushorts)
-            out << pair.first << " = " << *pair.second << "\n";
+            gameplay_entries.emplace(pair.first, std::to_string(*pair.second));
 
         std::map<LockDelayType, const std::string> locktype_to_str;
         for (const auto& pair : str_to_locktype)
             locktype_to_str.emplace(pair.second, pair.first);
         assert(locktype_to_str.count(well.lock_delay_type));
-        out << "lock_type = " << locktype_to_str.at(well.lock_delay_type) << "\n";
+        gameplay_entries.emplace("lock_type", locktype_to_str.at(well.lock_delay_type));
 
         std::map<RotationStyle, const std::string> rotation_to_str;
         for (const auto& pair : str_to_rotation)
             rotation_to_str.emplace(pair.second, pair.first);
         assert(rotation_to_str.count(well.rotation_style));
-        out << "rotation = " << rotation_to_str.at(well.rotation_style) << "\n";
+        gameplay_entries.emplace("rotation", rotation_to_str.at(well.rotation_style));
+
+        config.emplace("gameplay", std::move(gameplay_entries));
     }
+    ConfigFile::save(config, path);
 }
 
 std::tuple<SysConfig, WellConfig> GameConfigFile::load(const std::string& path)
 {
-    const std::regex valid_head(R"(\[[a-zA-Z0-9\.-_]+\])");
-    const std::regex valid_data(R"([a-z_]+\s*=\s*([0-9]{1,3}|on|off|"[a-z]+"))");
-    const std::regex whitespace(R"(\s+)");
+    const auto& config = ConfigFile::load(path);
+    if (config.empty())
+        return {};
+
+    const std::regex valid_value(R"(([0-9]{1,3}|on|off|[a-z]+))");
+    const std::set<std::string> accepted_headers = {"system", "gameplay"};
 
     SysConfig sys;
     WellConfig well;
-
-    const std::vector<std::string> accepted_headers = {"system", "gameplay"};
-    uint8_t current_header_idx = 0xFF;
 
     auto sys_bools = createBoolBind(sys);
     auto well_bools = createBoolBind(well);
     auto well_ushorts = createNumericBind(well);
 
-    std::ifstream infile(path);
-    unsigned linenum = 0;
-    std::string line;
-    while (std::getline(infile, line)) {
-        linenum++;
-        if (line.empty() || line.front() == '#')
+    for (const auto& block : config) {
+        const auto& block_name = block.first;
+        if (!accepted_headers.count(block_name)) {
+            Log::warning(LOG_TAG) << path << ": Unknown settings block '" << block_name << "', skipped\n";
             continue;
-
-        if (std::regex_match(line, valid_head)) {
-            auto current_head = line.substr(1, line.length() - 2);
-            assert(!current_head.empty()); // the regex requires at least one char
-            for (unsigned i = 0; i < accepted_headers.size(); i++) {
-                if (accepted_headers.at(i) == current_head) {
-                    current_header_idx = i;
-                    break;
-                }
-            }
         }
-        else if (std::regex_match(line, valid_data)) {
-            if (current_header_idx == 0xFF) {
-                Log::warning(LOG_TAG) << path << ":" << linenum << ": No known section header defined before this line\n";
-                Log::warning(LOG_TAG) << "Using default settings\n";
-                return std::make_tuple<SysConfig, WellConfig>({}, {});
+
+        for (const auto& keyval : block.second) {
+            const auto& key_str = keyval.first;
+            const auto& val_str = keyval.second;
+
+            if (!std::regex_match(val_str, valid_value)) {
+                Log::warning(LOG_TAG) << path << ": Unknown value '" << val_str
+                                              << "' for '" << key_str << "'\n";
+                Log::warning(LOG_TAG) << path << ": under '" << block_name << "', skipped\n";
+                continue;
             }
 
-            line = std::regex_replace(line, whitespace, "");
-            const auto split_pos = line.find("=");
-            const auto key = line.substr(0, split_pos);
-            const auto str_val = line.substr(split_pos + 1);
+            try {
+                if (sys_bools.count(key_str))
+                    parseBool(*sys_bools.at(key_str), key_str, val_str);
+                else if (well_bools.count(key_str))
+                    parseBool(*well_bools.at(key_str), key_str, val_str);
+                else if (well_ushorts.count(key_str)) {
+                    try {
+                        auto value = std::stoul(val_str);
+                        if (value > 0xFFFF)
+                            throw std::out_of_range("");
 
-
-            if (sys_bools.count(key)) {
-                try { parseBool(*sys_bools.at(key), key, str_val); }
-                catch (const std::runtime_error& err) {
-                    Log::warning(LOG_TAG) << path << ":" << linenum << ": " << err.what() << "\n";
-                }
-            }
-            else if (well_bools.count(key)) {
-                try { parseBool(*well_bools.at(key), key, str_val); }
-                catch (const std::runtime_error& err) {
-                    Log::warning(LOG_TAG) << path << ":" << linenum << ": " << err.what() << "\n";
-                }
-            }
-            else if (well_ushorts.count(key)) {
-                try {
-                    auto value = std::stoul(str_val);
-                    if (value > 0xFFFF)
-                        throw std::out_of_range("");
-
-                    *well_ushorts.at(key) = value;
-                }
-                catch (...) {
-                    Log::warning(LOG_TAG) << path << ":" << linenum
-                        << ": Invalid numeric value '" << str_val << "', ignored\n";
-                }
-            }
-            else if (accepted_wellenum_keys.count(key)) {
-                if (key == "lock_type") {
-                    if (str_to_locktype.count(str_val))
-                        well.lock_delay_type = str_to_locktype.at(str_val);
-                    else {
-                        Log::warning(LOG_TAG) << path << ":" << linenum
-                        << ": Invalid lock type value '" << str_val << "', ignored\n";
+                        *well_ushorts.at(key_str) = value;
+                    }
+                    catch (...) {
+                        throw std::runtime_error("Invalid numeric value '" + val_str + "', skipped");
                     }
                 }
-                else if (key == "rotation") {
-                    if (str_to_rotation.count(str_val))
-                        well.rotation_style = str_to_rotation.at(str_val);
-                    else {
-                        Log::warning(LOG_TAG) << path << ":" << linenum
-                        << ": Invalid rotation style value '" << str_val << "', ignored\n";
+                else if (accepted_wellenum_keys.count(key_str)) {
+                    if (key_str == "lock_type") {
+                        if (str_to_locktype.count(val_str))
+                            well.lock_delay_type = str_to_locktype.at(val_str);
+                        else
+                            throw std::runtime_error("Invalid lock type value '" + val_str + "', skipped");
+                    }
+                    else if (key_str == "rotation") {
+                        if (str_to_rotation.count(val_str))
+                            well.rotation_style = str_to_rotation.at(val_str);
+                        else
+                            throw std::runtime_error("Invalid rotation style value '" + val_str + "', skipped");
                     }
                 }
+                else
+                    throw std::runtime_error("Unknown option '" + key_str + "', ignored");
             }
-            else {
-                Log::warning(LOG_TAG) << path << ":" << linenum
-                << ": Unknown option '" << key << "' for section '"
-                << accepted_headers.at(current_header_idx) << "', ignored\n";
+            catch (const std::runtime_error& err) {
+                Log::warning(LOG_TAG) << path << ": " << err.what() << "\n";
+                Log::warning(LOG_TAG) << path << ": under '" << block_name << "'\n";
             }
-        }
-        else {
-            Log::warning(LOG_TAG) << path << ":" << linenum << ": Syntax error\n" << line;
-            Log::warning(LOG_TAG) << "Using default settings\n";
-            return std::make_tuple<SysConfig, WellConfig>({}, {});
         }
     }
 
