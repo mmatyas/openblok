efcbb7cd3f32ce8bf30cb3f8e2f11d5e129ec8fb
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-12-01 19:17:51 +0100

    Use hashmaps for input event and button maps

--- src/system/InputMap.h
+++ src/system/InputMap.h
@@ -5,12 +5,13 @@
 #include <stdint.h>
 #include <map>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 
 using DeviceName = const std::string;
-using EventToButtonsMap = std::map<InputType, std::vector<uint16_t>>;
-using ButtonToEventsMap = std::map<uint16_t, std::vector<InputType>>;
+using EventToButtonsMap = std::unordered_map<InputType, std::vector<uint16_t>, InputTypeHash>;
+using ButtonToEventsMap = std::unordered_map<uint16_t, std::vector<InputType>>;
 
 enum class DeviceType : uint8_t {
     KEYBOARD,
