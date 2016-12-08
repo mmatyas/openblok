bf4dfb7b0e55dc9e9b0b72fa0d286d51bf51a8a1
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-12-08 18:34:09 +0100

    Added gamepad connection popup icon to main menu

--- src/game/states/MainMenuState.cpp
+++ src/game/states/MainMenuState.cpp
@@ -3,10 +3,17 @@
 #include "game/AppContext.h"
 #include "substates/mainmenu/Base.h"
 #include "system/AudioContext.h"
+#include "system/Paths.h"
+#include "system/Texture.h"
 
 
 MainMenuState::MainMenuState(AppContext& app)
+    : padconnect_phase1(std::chrono::seconds(3), [](double){}, [this](){ padconnect_phase2.restart(); })
+    , padconnect_phase2(std::chrono::seconds(3), [](double t){ return (1.0 - t) * 0xFF; })
+    , tex_padconnected(app.gcx().loadTexture(Paths::data() + "gamepad-connect.png"))
 {
+    padconnect_phase1.stop();
+    padconnect_phase2.update(padconnect_phase2.length()); // set alpha to 0
     states.emplace_back(std::make_unique<SubStates::MainMenu::Base>(*this, app));
 }
 
@@ -15,27 +22,50 @@ MainMenuState::~MainMenuState() = default;
 void MainMenuState::update(const std::vector<Event>& events, AppContext& app)
 {
     for (const auto& event : events) {
-        if (event.type == EventType::WINDOW) {
-            switch (event.window) {
-                case WindowEvent::RESIZED:
-                    for (auto& state : states)
-                        state->updatePositions(app.gcx());
-                    break;
-                case WindowEvent::FOCUS_LOST:
-                    app.audio().pauseAll();
-                    break;
-                case WindowEvent::FOCUS_GAINED:
-                    app.audio().resumeAll();
-                    break;
-                default:
-                    break;
-            }
+        switch (event.type) {
+            case EventType::WINDOW:
+                switch (event.window) {
+                    case WindowEvent::RESIZED:
+                        for (auto& state : states)
+                            state->updatePositions(app.gcx());
+                        break;
+                    case WindowEvent::FOCUS_LOST:
+                        app.audio().pauseAll();
+                        break;
+                    case WindowEvent::FOCUS_GAINED:
+                        app.audio().resumeAll();
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case EventType::DEVICE:
+                switch (event.device.type) {
+                    case DeviceEventType::CONNECTED:
+                        padconnect_phase1.restart();
+                        padconnect_phase2.restart();
+                        padconnect_phase2.pause();
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
         }
     }
+
+    padconnect_phase1.update(Timing::frame_duration);
+    padconnect_phase2.update(Timing::frame_duration);
+    tex_padconnected->setAlpha(padconnect_phase2.value());
+
+    // this may pop the main menu state
     states.back()->update(*this, events, app);
 }
 
 void MainMenuState::draw(GraphicsContext& gcx)
 {
     states.back()->draw(*this, gcx);
+    tex_padconnected->drawAt(gcx.screenWidth() - 30 - tex_padconnected->width(),
+                             gcx.screenHeight() - 30 - tex_padconnected->height());
 }
--- src/game/states/MainMenuState.h
+++ src/game/states/MainMenuState.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "game/GameState.h"
+#include "game/Transition.h"
 
 #include <list>
 #include <memory>
@@ -16,4 +17,9 @@ public:
     void draw(GraphicsContext&) final;
 
     std::list<std::unique_ptr<SubStates::MainMenu::State>> states;
+
+private:
+    Transition<void> padconnect_phase1; ///< phase 1: the popup is visible for a few seconds
+    Transition<uint8_t> padconnect_phase2; ///< phase 2: the popup disappears in a few seconds
+    std::unique_ptr<Texture> tex_padconnected;
 };
