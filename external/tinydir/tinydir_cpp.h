cbf11f22b8c59e0751a47deb0fed465a89fc40d8
Mátyás Mustoha <matyas.mustoha@gmail.com>
2017-03-08 19:06:04 +0100

    Fixed MinGW build error

--- external/tinydir/tinydir_cpp.h
+++ external/tinydir/tinydir_cpp.h
@@ -10,11 +10,15 @@
 #define WIN32_LEAN_AND_MEAN
 #include <io.h>
 #define posix_access _access
-#undef INFINITE
 #else
 #include <unistd.h>
 #define posix_access access
 #endif
+
+#if defined _MSC_VER || defined __MINGW32__
+#undef INFINITE
+#endif
+
 
 inline bool path_exists(const std::string& path) {
     return posix_access(path.c_str(), 0) == 0;
